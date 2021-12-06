cd3c61523c02390feb77606591c5847a5fe4fcd8
NinaWie <wnina@student.ethz.ch>
2021-12-06 18:51:33 +0100

    refactor: include node filtering and feature matrix in dataset class, move functions to utils

--- predict_visits/dataset.py
+++ predict_visits/dataset.py
@@ -1,3 +1,4 @@
+from scipy.sparse import coo
 import torch
 import pickle
 import os
@@ -5,51 +6,78 @@ import scipy.sparse as sp
 import numpy as np
 from torch.functional import _return_counts, norm
 
+from utils import *
+
 
 class MobilityGraphDataset(torch.utils.data.Dataset):
+    """
+    Dataset to yield graph and test nodes separately
+    -> create new dataset if we want to do link prediction / other pipeline
+
+    Arguments:
+        ratio_predict: Ratio of nodes per graph that is left out for training
+        quantile_lab: Only the nodes in the lower x-quantile are left out for
+            training (e.g. home node can not be left out)
+        nr_keep: graphs are reduced to the x nodes with highest degree
+    """
+
     def __init__(
-        self, path=os.path.join("data", "dataset.pkl"), ratio_predict=0.1
+        self,
+        path=os.path.join("data", "dataset.pkl"),
+        ratio_predict=0.1,
+        quantile_lab=0.95,
+        nr_keep=50,
     ):
         """
         Data Loader for mobility graphs
         """
+        self.nr_keep = nr_keep
+        # Load data - Note: adjacency is a list of adjacency matrices, and
+        # coordinates is a list of arrays (one for each user)
         with open(path, "rb") as infile:
-            (self.users, adjacency_raw, node_feats) = pickle.load(infile)
+            (self.users, adjacency_graphs, coordinates_graphs) = pickle.load(
+                infile
+            )
 
-        node_feats, _ = self.node_feature_preprocessing(node_feats)
+        # Preprocess the graph to get adjacency and node features
+        node_feats, adjacency = self.graph_preprocessing(
+            adjacency_graphs,
+            coordinates_graphs,
+            quantile_lab=quantile_lab,
+            nr_keep=nr_keep,
+        )
+        # store the feature dimension
+        self.num_feats = node_feats[0].shape[1]
 
-        # print(np.mean(np.absolute(node_feats[0]), axis=0))
-        # print()
-        # all_labels = [e[-1] for f in node_feats for e in f]
-        # print(np.unique(all_labels, return_counts=True))
-
-        # leave some out for prediction task
+        # Split into graph (adjacency and known node feats) and "new" locations
+        # (predict_node_feats) with their visit numbers (labels)
         self.adjacency, self.known_node_feats = [], []
         self.predict_node_feats, self.labels = [], []
         for i in range(len(self.users)):
+            feat_vector = node_feats[i]
+
+            # Leave out some nodes from the graph --> only take the ones that
+            # are visited less frequently --> labs preprocessed such that cutoff
+            # is at 1
+            possible_nodes = np.where(feat_vector[:, -1] <= 1)[0]
+            rand_perm = np.random.permutation(possible_nodes)
             # we use ratio_predict percent of the nodes to predict
-            num_nodes_this_graph = len(node_feats[i])
-            number_nodes_predict = int(num_nodes_this_graph * ratio_predict)
-            # TODO: exclude two nodes with highest number of visits --> outlier
-            rand_perm = np.random.permutation(num_nodes_this_graph)
+            number_nodes_predict = int(len(feat_vector) * ratio_predict)
             # divide in known nodes and the ones to predict:
             predict_nodes = rand_perm[:number_nodes_predict]
-            known_nodes = rand_perm[number_nodes_predict:]
+            # known nodes are also the rest (that is not in possible_nodes)
+            known_nodes = list(rand_perm[number_nodes_predict:]) + list(
+                np.where(feat_vector[:, -1] > 1)[0]
+            )
+
             # reduce node feats to known nodes
-            self.known_node_feats.append(node_feats[i][known_nodes])
-            self.predict_node_feats.append(node_feats[i][predict_nodes, :-1])
-            self.labels.append(node_feats[i][predict_nodes, -1])
-            # print(
-            #     "appended predict node feats", self.predict_node_feats[-1].shape
-            # )
-            # print("appended label", self.labels[-1])
-            # print("appended known node feats", self.known_node_feats[-1].shape)
-            # reduce adjacency matrix to known nodes
-            adj = adjacency_raw[i][known_nodes]
+            self.known_node_feats.append(feat_vector[known_nodes])
+            self.predict_node_feats.append(feat_vector[predict_nodes, :-1])
+            self.labels.append(feat_vector[predict_nodes, -1])
+
+            # reduce adjacency matrix to known nodes and preprocess
+            adj = adjacency[i][known_nodes]
             adj = adj[:, known_nodes]
-            # print("adj shape prev", adjacency_raw[i].shape)
-            # print("remove nodes", len(predict_nodes))
-            # print("adj shape after", adj.shape)
             self.adjacency.append(self.adjacency_preprocessing(adj).float())
 
         self.avail_predict_nodes = [len(arr) for arr in self.labels]
@@ -58,9 +86,75 @@ class MobilityGraphDataset(torch.utils.data.Dataset):
 
     @staticmethod
     def adjacency_preprocessing(adjacency_matrix):
+        """
+        Preprocess the adjacency matrix - return unweighted normalized tensor
+        """
+        unweighted_adjacency = (adjacency_matrix > 0).astype(int)
         return sparse_mx_to_torch_sparse_tensor(
-            preprocess_adj(adjacency_matrix)
+            preprocess_adj(unweighted_adjacency)
         )
+
+    @staticmethod
+    def node_feature_preprocessing(home_node, coordinates):
+        # subtract the home location
+        home_center = coordinates[home_node]
+        displacement = coordinates - home_center
+        # normalize distance by applying log and dividing by std
+        temp_sign = np.sign(displacement)
+        normed_displacement = temp_sign * np.log(np.absolute(displacement) + 1)
+        normed_displacement = normed_displacement / np.std(normed_displacement)
+        # add other features
+        # dist = ti.geogr.point_distances.haversine_dist(
+        #         center.x, center.y, home_center.x, home_center.y
+        #     )[0]
+        # TODO
+        feature_matrix = normed_displacement
+        return feature_matrix
+
+    @staticmethod
+    def graph_preprocessing(
+        adjacency_graphs, coordinates_graphs, quantile_lab=0.9, nr_keep=50
+    ):
+        """Preprocess the node features of the graph"""
+        node_feats, adjacency_matrices = [], []
+        for adjacency, coordinates in zip(adjacency_graphs, coordinates_graphs):
+            home_node = get_home_node(adjacency)
+
+            # 1) crop or pad adjacency matrix to the x nodes with highest degree
+            unweighted_adj = (adjacency > 0).astype(int)
+            overall_degree = (
+                np.array(np.sum(unweighted_adj, axis=0))[0]
+                + np.array(np.sum(unweighted_adj, axis=1))[0]
+            )
+            use_nodes = np.argsort(overall_degree)[-nr_keep:]
+            adj_new = crop_pad_sparse_matrix(adjacency, use_nodes, nr_keep)
+            adjacency_matrices.append(adj_new)
+
+            # 2) process node features
+            # transform geographic coordinates and make feature matrix
+            feature_matrix = MobilityGraphDataset.node_feature_preprocessing(
+                home_node, coordinates
+            )
+            # restrict to use_nodes and pad (to align indices with adjacency)
+            feature_matrix = feature_matrix[use_nodes]
+            feature_matrix = np.pad(
+                feature_matrix,
+                ((0, nr_keep - feature_matrix.shape[0]), (0, 0)),
+            )
+
+            # 3) Get label (number of visits)
+            # get the weighted in degree (the label in the prediction task)
+            label = get_label(adj_new)
+            # normalize label by the quantile
+            label_cutoff = max([np.quantile(label, quantile_lab), 1])
+            label = label / label_cutoff
+
+            # concatenate the other features with the labels and append
+            concat_feats_labels = np.concatenate(
+                (feature_matrix, np.expand_dims(label, 1)), axis=1
+            )
+            node_feats.append(concat_feats_labels)
+        return node_feats, adjacency_matrices
 
     def __len__(self):
         return self.nr_graphs
@@ -74,92 +168,13 @@ class MobilityGraphDataset(torch.utils.data.Dataset):
             self.labels[idx][rand_label_ind],
         )
 
-    @staticmethod
-    def node_feature_preprocessing(node_feats, dist_stats=None):
-        if dist_stats is not None:
-            dist_mean_list, dist_std_list, lab_max_list = dist_stats
-        else:
-            dist_mean_list, dist_std_list, lab_max_list = [], [], []
-        # normalize node feats:
-        # normalize the distances also per user --> we get a relation to
-        # the user's own perceived distance
-        normed_node_feats_list = []
-        for i in range(len(node_feats)):
-            normed_node_feats = node_feats[i].copy()
-            # print("prev", normed_node_feats[:10])
-            # get sign (mainly of coordinate features)
-            temp_sign = np.sign(normed_node_feats)
-            # 1) Apply log because distances and visits both follow power law
-            normed_node_feats = temp_sign * np.log(
-                np.absolute(normed_node_feats) + 1
-            )
-            # compute stats that must be saved for the evaluation
-            if dist_stats is None:
-                dist_means = np.mean(normed_node_feats[:, :-1], axis=0)
-                dist_stds = np.std(normed_node_feats[:, :-1], axis=0)
-                lab_max = np.max(normed_node_feats[:, -1])
-                dist_mean_list.append(dist_means)
-                dist_std_list.append(dist_stds)
-                lab_max_list.append(lab_max)
-            else:
-                dist_means, dist_stds, lab_max = (
-                    dist_mean_list[i],
-                    dist_std_list[i],
-                    lab_max_list[i],
-                )
-
-            # 2) Normalize labels (#visits) by max because prediction should be
-            # time-period independent
-            normed_node_feats[:, -1] = normed_node_feats[:, -1] / lab_max
-            # 3) normalize distances by ?? Median / mean and std
-            normed_node_feats[:, :-1] = (
-                normed_node_feats[:, :-1] - dist_means
-            ) / dist_stds
-            assert node_feats[i].shape == normed_node_feats.shape
-            normed_node_feats_list.append(normed_node_feats)
-            # print("after", normed_node_feats[:10])
-        if dist_stats is None:
-            return normed_node_feats_list, (
-                dist_mean_list,
-                dist_std_list,
-                lab_max_list,
-            )
-        return normed_node_feats_list
-
-
-def normalize_adj(adj):
-    """From tkipf: Symmetrically normalize adjacency matrix."""
-    adj = sp.coo_matrix(adj)
-    rowsum = np.array(adj.sum(1))
-    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.0
-    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
-
-
-def preprocess_adj(adj):
-    """From tkipf: Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
-    adj_normalized = normalize_adj(adj + sp.eye(adj.shape[0]))
-    return adj_normalized
-
-
-def sparse_mx_to_torch_sparse_tensor(sparse_mx):
-    """From tkipf: Convert a scipy sparse matrix to a torch sparse tensor."""
-    sparse_mx = sparse_mx.tocoo().astype(np.float32)
-    indices = torch.from_numpy(
-        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64)
-    )
-    values = torch.from_numpy(sparse_mx.data)
-    shape = torch.Size(sparse_mx.shape)
-    return torch.sparse.FloatTensor(indices, values, shape)
-
 
 if __name__ == "__main__":
     data = MobilityGraphDataset("data/train_data.pkl")
     counter = 0
     for (adj, nf, pnf, lab) in data:
-        print(nf, lab)
-        # print(adj.size(), nf.shape, pnf.shape, lab)
+        print(nf)
+        print(adj.size(), nf.shape, pnf.shape, lab)
         counter += 1
-        if counter > 3:
+        if counter > 10:
             break
---
+++ predict_visits/utils.py
@@ -0,0 +1,119 @@
+import numpy as np
+import torch
+import scipy.sparse as sp
+
+
+def get_home_node(adjacency):
+    # find home node
+    out_degrees = np.array(np.sum(adjacency, axis=1))
+    return np.argmax(out_degrees)
+
+
+def get_label(adjacency):
+    # get the indegrees
+    return np.array(np.sum(adjacency, axis=0))[0]
+
+
+def normalize_adj(adj):
+    """From tkipf: Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.0
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+
+
+def preprocess_adj(adj):
+    """From tkipf: Preprocessing of adjacency matrix for simple GCN model and conversion to tuple representation."""
+    adj_normalized = normalize_adj(adj + sp.eye(adj.shape[0]))
+    return adj_normalized
+
+
+def sparse_mx_to_torch_sparse_tensor(sparse_mx):
+    """From tkipf: Convert a scipy sparse matrix to a torch sparse tensor."""
+    sparse_mx = sparse_mx.tocoo().astype(np.float32)
+    indices = torch.from_numpy(
+        np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64)
+    )
+    values = torch.from_numpy(sparse_mx.data)
+    shape = torch.Size(sparse_mx.shape)
+    return torch.sparse.FloatTensor(indices, values, shape)
+
+
+def crop_pad_sparse_matrix(adjacency, use_nodes, keep_nodes):
+    """
+    Reduce sparse matrix to the indices use_nodes or pad such that the number
+    of rows and columns is keep_nodes
+    """
+    adj_temp = adjacency[use_nodes]
+    adj_temp = adj_temp[:, use_nodes]
+    diff = keep_nodes - adj_temp.shape[0]
+    if diff > 0:
+        from scipy.sparse import vstack, hstack
+
+        adj_temp = hstack((adj_temp, np.zeros((adj_temp.shape[0], diff))))
+        adj_temp = vstack((adj_temp, np.zeros((diff, 50)))).tocsr()
+    return adj_temp
+
+
+def old_node_preprocessing(node_feats, dist_stats=None):
+    if dist_stats is not None:
+        dist_mean_list, dist_std_list, lab_max_list = dist_stats
+    else:
+        dist_mean_list, dist_std_list, lab_max_list = [], [], []
+    # normalize node feats:
+    # normalize the distances also per user --> we get a relation to
+    # the user's own perceived distance
+    normed_node_feats_list = []
+    for i in range(len(node_feats)):
+        normed_node_feats = node_feats[i].copy()
+        # print("prev", normed_node_feats[:10])
+        # get sign (mainly of coordinate features)
+        temp_sign = np.sign(normed_node_feats)
+        # 1) Apply log because distances and visits both follow power law
+        normed_node_feats = temp_sign * np.log(
+            np.absolute(normed_node_feats) + 1
+        )
+        # compute stats that must be saved for the evaluation
+        if dist_stats is None:
+            # note: dist_means is not used currently
+            dist_means = np.mean(normed_node_feats[:, :-1], axis=0)
+            dist_stds = np.std(normed_node_feats[:, :-1], axis=0)
+            lab_max = np.max(normed_node_feats[:, -1])
+            dist_mean_list.append(dist_means)
+            dist_std_list.append(dist_stds)
+            lab_max_list.append(lab_max)
+        else:
+            dist_means, dist_stds, lab_max = (
+                dist_mean_list[i],
+                dist_std_list[i],
+                lab_max_list[i],
+            )
+        # # SANITY CHECK: can we predict just the normalized distance?
+        # # Note: need to comment lab max in code above
+        # normed_node_feats[:, -1] = normed_node_feats[:, 0] / dist_means[0]
+        # if dist_stats is None:
+        #     lab_max = np.max(normed_node_feats[:, -1])
+        #     lab_max_list.append(lab_max)
+        # else:
+        #     lab_max = lab_max_list[i]
+        # normed_node_feats[:, -1] = normed_node_feats[:, -1] / lab_max
+
+        # 2) Normalize labels (#visits) by max because prediction should be
+        # time-period independent
+        normed_node_feats[:, -1] = normed_node_feats[:, -1] / lab_max
+        # 3) normalize distances by ?? Median / mean and std
+        # TODO: subtract mean dist does not make sense because then home is
+        # at -3
+        normed_node_feats[:, :-1] = (normed_node_feats[:, :-1]) / dist_stds
+        assert node_feats[i].shape == normed_node_feats.shape
+        normed_node_feats_list.append(normed_node_feats)
+        # print("after", normed_node_feats[:10])
+    if dist_stats is None:
+        return normed_node_feats_list, (
+            dist_mean_list,
+            dist_std_list,
+            lab_max_list,
+        )
+    return normed_node_feats_list
