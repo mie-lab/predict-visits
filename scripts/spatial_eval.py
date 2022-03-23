2d463027d6d7fa68f677177ae54b783cd38e05f0
NinaWie <wnina@student.ethz.ch>
2022-03-23 11:50:34 +0100

    spatial distribution plot

--- scripts/spatial_eval.py
+++ scripts/spatial_eval.py
@@ -11,6 +11,8 @@ import torch
 from predict_visits.model.graph_resnet import VisitPredictionModel
 from predict_visits.dataset import MobilityGraphDataset
 from predict_visits.utils import get_label, load_model
+from predict_visits.config import model_dict
+from predict_visits.model.transforms import NoTransform
 
 
 def select_node(node_feats, min_label=1, max_label=10):
@@ -41,7 +43,7 @@ def regular_grid(coordinates):
         np.std(coordinates[:, 0]),
         np.std(coordinates[:, 1]),
     )
-    factor_std = 3
+    factor_std = 1
     min_x, max_x = (-1 * factor_std * std_x, factor_std * std_x)
     min_y, max_y = (-1 * factor_std * std_y, factor_std * std_y)
     x_range = np.linspace(min_x, max_x, 20)
@@ -54,48 +56,81 @@ def regular_grid(coordinates):
     return test_node_arr
 
 
-def visualize_grid(node_feats_inp, inp_adj, inp_graph_nodes):
-    """
-    TODO: adapt to new feature representation
-    TODO: heatmap
-    """
-    grid_locations = regular_grid(node_feats_inp)
-    inp_test_nodes = torch.from_numpy(grid_locations).float()
+def create_fake_nodes(node_feat_df):
+    loc_grid = regular_grid(np.array(node_feat_df[["x_normed", "y_normed"]]))
+    fake_nodes = pd.DataFrame(loc_grid, columns=["x_normed", "y_normed"])
+    fake_nodes["distance"] = np.sqrt(
+        fake_nodes["x_normed"].values ** 2 + fake_nodes["y_normed"].values ** 2
+    )
+    fake_nodes["purpose"] = "unknown"
+    return fake_nodes
 
-    labels_for_graph = []
-    for k in range(len(grid_locations)):
-        lab = model(inp_graph_nodes, inp_adj, inp_test_nodes[k])
-        labels_for_graph.append(lab.item())
 
-    # ------ Visualization ------------
-    # normalize the predictions
-    labels_for_graph = np.array(labels_for_graph)
-    labels_for_graph_normed = (labels_for_graph - np.min(labels_for_graph)) / (
-        np.max(labels_for_graph) - np.min(labels_for_graph)
+def plot_spatial_distribution_3d(
+    grid_locations,
+    grid_labels,
+    user_locations,
+    user_labels,
+    max_label=10,
+    save_path="outputs/plot3d.png",
+):
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection="3d")
+    x, y = np.swapaxes(grid_locations, 1, 0)
+    z = grid_labels
+    ax.plot_trisurf(x, y, z)  # scatter plot_trisurf
+    x, y = np.swapaxes(user_locations, 1, 0)
+    z = user_labels
+    ax.scatter(x, y, z, c="red")
+    plt.xlabel("X coordinate (relative to home)")
+    plt.ylabel("Y coordinate (relative to home)")
+    ax.set_zlabel("Number of visits")
+    ax.set_zlim(0, max_label)
+    plt.savefig(save_path)
+    # plt.show()
+
+
+def plot_spatial_distribution_2d(
+    grid_locations,
+    grid_labels,
+    user_locations,
+    user_labels,
+    save_path="outputs/plot2d.png",
+):
+
+    grid_labels_normed = (grid_labels - np.min(grid_labels)) / (
+        np.max(grid_labels) - np.min(grid_labels)
     ) + 0.1
-    plt.figure(figsize=(20, 10))
+    user_labels_normed = (user_labels - np.min(user_labels)) / (
+        np.max(user_labels) - np.min(user_labels)
+    ) + 0.1
+
+    plt.figure(figsize=(7, 7))
     # scatter the locations in the original user graph
     plt.scatter(
-        node_feats_inp[:, 0],
-        node_feats_inp[:, 1],
-        s=node_feats_inp[:, -1] * 100,
+        user_locations[:, 0],
+        user_locations[:, 1],
+        s=user_labels_normed * 100,
     )
     #  scatter the grid-layout locations with point size proportional to
     # the predicted label
     plt.scatter(
-        grid_locations[i][:, 0],
-        grid_locations[i][:, 1],
-        s=labels_for_graph_normed * 100,
+        grid_locations[:, 0],
+        grid_locations[:, 1],
+        s=grid_labels_normed * 100,
     )
     plt.xlim(
-        np.min(grid_locations[i][:, 0]),
-        np.max(grid_locations[i][:, 0]),
+        np.min(grid_locations[:, 0]),
+        np.max(grid_locations[:, 0]),
     )
     plt.ylim(
-        np.min(grid_locations[i][:, 1]),
-        np.max(grid_locations[i][:, 1]),
+        np.min(grid_locations[:, 1]),
+        np.max(grid_locations[:, 1]),
     )
-    plt.savefig(os.path.join(out_path, f"grid_pred_{i}.png"))
+    plt.xlabel("X coordinate (relative to home)")
+    plt.ylabel("Y coordinate (relative to home)")
+    plt.savefig(save_path)
+    # plt.show()
 
 
 if __name__ == "__main__":
@@ -129,60 +164,86 @@ if __name__ == "__main__":
     MAX_LABEL = 10
 
     model, cfg = load_model(model_path)
+    model.eval()
+    cfg["include_poi"] = False
+
+    # add transform function
+    model_cfg = model_dict[cfg["model"]]
+    transform_for_model = model_cfg.get("inp_transform", NoTransform)(**cfg)
 
     with open(os.path.join("data", test_data_path), "rb") as infile:
         (users, adjacency_graphs, node_feat_list) = pickle.load(infile)
 
-    i = 0
+    for i in range(10):
 
-    node_feats_raw = node_feat_list[i]
-    adjacency_raw = adjacency_graphs[i]
-    take_out_ind = select_node(
-        node_feats_raw, min_label=MIN_LABEL, max_label=MAX_LABEL
-    )
-    use_nodes = np.delete(np.arange(len(node_feats_raw)), take_out_ind)
+        node_feats_raw = node_feat_list[i]
+        adjacency_raw = adjacency_graphs[i]
 
-    # crop to used nodes
-    node_feats_raw_graph = node_feats_raw.iloc[use_nodes]
-    adjacency_raw = adjacency_raw[use_nodes]
-    adjacency_raw = adjacency_raw[:, use_nodes]
+        # get fake nodes:
+        fake_node_df = create_fake_nodes(node_feats_raw)
 
-    input_node_raw = node_feats_raw.iloc[take_out_ind : take_out_ind + 1]
-    gt_label = input_node_raw["out_degree"].values[0]
-    # print(node_feats_raw_graph.shape, adjacency_raw.shape, input_node_raw.shape)
+        # preprocess graph
+        (
+            node_feats,
+            adj,
+            stats_and_cutoff,
+        ) = MobilityGraphDataset.graph_preprocessing(
+            adjacency_raw, node_feats_raw, **cfg
+        )
+        label_cutoff = stats_and_cutoff[1]
 
-    # preprocess graph
-    node_feats, adj, _ = MobilityGraphDataset.graph_preprocessing(
-        adjacency_raw, node_feats_raw_graph, **cfg
-    )
+        # preprocess the left out node:
+        input_node, _ = MobilityGraphDataset.node_feature_preprocessing(
+            fake_node_df, stats=stats_and_cutoff[0], **cfg
+        )
+        predict_node_feats = np.concatenate(
+            (input_node, np.zeros((len(input_node), 1))), axis=1
+        )
 
-    # preprocess labels and upper bound on labels
-    nr_visits_historic = get_label(adj)
-    label_cutoff = MobilityGraphDataset.prep_cutoff(
-        nr_visits_historic, cfg.get("label_cutoff", 0.95), cfg["log_labels"]
-    )
+        # create torch_geometric input data
+        grid_labels = []
+        for predict_node in predict_node_feats:
+            data = MobilityGraphDataset.transform_to_torch(
+                adj,
+                node_feats,
+                predict_node,
+                cfg["relative_feats"],
+                cfg.get("adj_is_unweighted", True),
+                cfg.get("adj_is_symmetric", True),
+                add_batch=True,
+            )
 
-    # preprocess the left out node:
-    input_node, _ = MobilityGraphDataset.node_feature_preprocessing(
-        input_node_raw, embedding=cfg.get("embedding", "simple")
-    )
-    # TODO: Modify the node in space or POIs!
+            # final model-dependent transform
+            inp_data = transform_for_model(data)
 
-    # create torch_geometric input data
-    print(input_node.shape)
-    assert input_node.shape[0] == 1
-    data = MobilityGraphDataset.transform_to_torch(
-        adj,
-        node_feats,
-        input_node[0],
-        cfg["relative_feats"],
-        add_batch=True,
-    )
+            # RUN MODEL
+            lab = data.y[:, -1]
+            pred = model(inp_data)
 
-    print(
-        data.x.size(),
-        data.y.size(),
-        data.edge_index.size(),
-        data.edge_attr.size(),
-        gt_label,
-    )
+            unnormed_pred = MobilityGraphDataset.unnorm_label(
+                pred.item(),
+                label_cutoff,
+                cfg.get("log_labels", False),
+            )
+            grid_labels.append(unnormed_pred)
+
+        grid_labels = np.array(grid_labels)
+        grid_locations = np.array(fake_node_df[["x_normed", "y_normed"]])
+        user_locations = np.array(node_feats_raw[["x_normed", "y_normed"]])
+        user_labels = np.array(node_feats_raw[["out_degree"]])
+
+        plot_spatial_distribution_2d(
+            grid_locations,
+            grid_labels,
+            user_locations,
+            user_labels,
+            save_path=os.path.join(f"outputs/spatial_eval/plot_2d_{i}"),
+        )
+
+        plot_spatial_distribution_3d(
+            grid_locations,
+            grid_labels,
+            user_locations,
+            user_labels,
+            save_path=os.path.join(f"outputs/spatial_eval/plot_3d_{i}"),
+        )
